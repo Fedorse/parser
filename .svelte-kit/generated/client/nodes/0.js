7c5d67356e640e412c7a688c925bd7b9105a98a6
Fedor Semochkin <221800ss@gmail.com>
2025-05-23 10:36:42 +0700

    added shad-cdn lybrary

--- .svelte-kit/generated/client/nodes/0.js
+++ .svelte-kit/generated/client/nodes/0.js
@@ -1,3 +1,3 @@
 import * as universal from "../../../../src/routes/+layout.ts";
 export { universal };
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.21.1_@sveltejs+vite-plugin-svelte@5.0.3_svelte@5.32.1_vite@6.3.5_jiti@1_b03a6730512f08e67b6c513f1f1cd160/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/layout.svelte";
+export { default as component } from "../../../../src/routes/+layout.svelte";
