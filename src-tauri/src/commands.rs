7cd6f98b04bf7dbd03992d4dec4fd6e2473728f9
Ivan Semochkin <ivansemochkin@Ivans-MacBook-Pro.local>
2025-05-23 16:37:32 +0700

    Cleanup errors

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,29 +1,77 @@
-use std::fs::{read_dir, read_to_string, remove_file, rename, File};
+use std::fs::{self, File};
+use std::fmt;
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
-use crate::utils::{FilePreview, create_output_file, get_file_preview, parse_paths, write_parsed_files, get_app_dir, get_presets_map, write_presets};
+use serde::{Deserialize, Serialize};
+use anyhow::{self, Result};
+
+use crate::utils::{self, FilePreview};
 use crate::consts::{PRESETS_FILE_NAME, PARSED_FILES_DIR};
 
-#[tauri::command]
-pub fn parse(paths: Vec<String>, ignore_patterns: Vec<String>) -> Result<(), String> {
-    let mut output_file = create_output_file().map_err(|e| e.to_string())?;
-    let parsed_paths = parse_paths(paths, ignore_patterns).map_err(|e| e.to_string())?;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CommandError {
+    message: String,
+}
 
-    write_parsed_files(parsed_paths, &mut output_file)?;
+impl fmt::Display for CommandError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl From<anyhow::Error> for CommandError {
+    fn from(err: anyhow::Error) -> Self {
+        Self {
+            message: err.to_string(),
+        }
+    }
+}
+
+impl From<std::io::Error> for CommandError {
+    fn from(err: std::io::Error) -> Self {
+        Self {
+            message: err.to_string(),
+        }
+    }
+}
+
+
+impl From<String> for CommandError {
+    fn from(err: String) -> Self {
+        Self {
+            message: err.to_string(),
+        }
+    }
+}
+
+impl From<&str> for CommandError {
+    fn from(err: &str) -> Self {
+        Self {
+            message: err.to_string(),
+        }
+    }
+}
+
+#[tauri::command]
+pub fn parse(paths: Vec<String>, ignore_patterns: Vec<String>) -> Result<(), CommandError> {
+    let mut output_file = utils::create_output_file()?;
+    let parsed_paths = utils::parse_paths(paths, ignore_patterns)?;
+
+    utils::write_parsed_files(parsed_paths, &mut output_file)?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn get_files() -> Result<Vec<FilePreview>, String> {
-    let parsed_files_dir = get_app_dir()?.join(PARSED_FILES_DIR);
+pub fn get_files() -> Result<Vec<FilePreview>, CommandError> {
+    let parsed_files_dir = utils::get_app_dir()?.join(PARSED_FILES_DIR);
     let mut previews = Vec::new();
 
-    for entry in read_dir(parsed_files_dir).map_err(|e| e.to_string())? {
-        let entry = entry.map_err(|e| e.to_string())?;
+    for entry in fs::read_dir(parsed_files_dir)? {
+        let entry = entry?;
         let file_path = entry.path();
-        let preview = get_file_preview(file_path).map_err(|e| e.to_string())?;
+        let preview = utils::get_file_preview(file_path)?;
         previews.push(preview);
     }
 
@@ -31,68 +79,68 @@ pub fn get_files() -> Result<Vec<FilePreview>, String> {
 }
 
 #[tauri::command]
-pub fn get_file_content(file_path: String) -> Result<String, String> {
-    let file_content: String = read_to_string(file_path).map_err(|e| e.to_string())?;
+pub fn get_file_content(file_path: String) -> Result<String, CommandError> {
+    let file_content: String = fs::read_to_string(file_path)?;
 
     Ok(file_content)
 }
 
 #[tauri::command]
-pub fn get_presets() -> Result<String, String> {
-    let presets_file = get_app_dir()?.join(PRESETS_FILE_NAME);
-    let presets: String = read_to_string(presets_file).map_err(|e| e.to_string())?;
+pub fn get_presets() -> Result<String, CommandError> {
+    let presets_file = utils::get_app_dir()?.join(PRESETS_FILE_NAME);
+    let presets = fs::read_to_string(presets_file)?;
 
     Ok(presets)
 }
 
 #[tauri::command]
-pub fn update_file(file_path: String, content: String) -> Result<(), String> {
-    let mut file = File::create(file_path).map_err(|e| e.to_string())?;
-    file.write_all(content.as_bytes()).map_err(|e| e.to_string())?;
+pub fn update_file(file_path: String, content: String) -> Result<(), CommandError> {
+    let mut file = File::create(file_path)?;
+    file.write_all(content.as_bytes())?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn rename_file(file_path: String, new_name: String) -> Result<(), String> {
+pub fn rename_file(file_path: String, new_name: String) -> Result<(), CommandError> {
     let old_path = PathBuf::from(&file_path);
     let new_path = old_path.parent().ok_or("Failed to get parrent dir")?.join(new_name);
 
-    rename(&old_path, &new_path).map_err(|e| e.to_string())?;
+    fs::rename(&old_path, &new_path)?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn delete_file(path: String) -> Result<(), String> {
-    let file_path = get_app_dir().map_err(|e| e.to_string())?.join(PARSED_FILES_DIR).join(path);
-    remove_file(file_path).map_err(|e| e.to_string())?;
+pub fn delete_file(path: String) -> Result<(), CommandError> {
+    let file_path = utils::get_app_dir()?.join(PARSED_FILES_DIR).join(path);
+    fs::remove_file(file_path)?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn update_preset(name: String, ignore_patterns: Vec<String>) -> Result<(), String> {
-    let mut presets = get_presets_map()?;
+pub fn update_preset(name: String, ignore_patterns: Vec<String>) -> Result<(), CommandError> {
+    let mut presets = utils::get_presets_map()?;
 
     presets.insert(name, ignore_patterns);
-    write_presets(&presets)?;
+    utils::write_presets(&presets)?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn delete_preset(name: String) -> Result<(), String> {
-    let mut presets = get_presets_map()?;
+pub fn delete_preset(name: String) -> Result<(), CommandError> {
+    let mut presets = utils::get_presets_map()?;
 
     presets.remove(&name);
-    write_presets(&presets)?;
+    utils::write_presets(&presets)?;
 
     Ok(())
 }
 
 #[tauri::command]
-pub fn open_in_folder(file_path: String) -> Result<(), String> {
+pub fn open_in_folder(file_path: String) -> Result<(), CommandError> {
     let path = std::path::Path::new(&file_path);
 
     #[cfg(target_os = "macos")]
@@ -128,7 +176,7 @@ pub fn open_in_folder(file_path: String) -> Result<(), String> {
 }
 
 #[tauri::command]
-pub fn open_in_default_editor(file_path: String) -> Result<(), String> {
+pub fn open_in_default_editor(file_path: String) -> Result<(), CommandError> {
         #[cfg(target_os = "windows")]
         {
             Command::new("cmd")
--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,73 +1,73 @@
+use crate::consts::{
+    APP_NAME, DEFAULT_PRESETS_PATH, PARSED_FILES_DIR, PRESETS_FILE_NAME, PREVIEW_LINE_LIMIT,
+};
+use anyhow;
+use chrono::Local;
+use dirs;
+use globset::{Glob, GlobSetBuilder};
+use serde::Serialize;
+use serde_json;
 use std::{
     collections::HashMap,
-    error::Error,
-    io::{Write},
-    path::{PathBuf, Path},
-    fs::{create_dir, read_dir, read, read_to_string, File, write, metadata},
-    process::Command
+    fs::{self, File},
+    io::Write,
+    path::{Path, PathBuf},
+    process::Command,
 };
-use serde::Serialize;
-use serde_json::{from_str, to_string_pretty};
-use chrono::Local;
-use globset::{Glob, GlobSetBuilder};
-use dirs::home_dir;
-use crate::consts::{APP_NAME, PARSED_FILES_DIR, PRESETS_FILE_NAME, DEFAULT_PRESETS_PATH, PREVIEW_LINE_LIMIT};
 
 #[derive(Serialize)]
 pub struct FilePreview {
     path: String,
     name: String,
     preview: String,
-    size: u64
+    size: u64,
 }
 
-pub fn init_app_structure() -> Result<(), Box<dyn Error>> {
-    let app_dir = home_dir().ok_or("Failed to get home directory")?.join(APP_NAME);
+pub fn init_app_structure() -> anyhow::Result<()> {
+    let app_dir = get_app_dir()?;
 
     if app_dir.exists() {
         return Ok(());
     }
 
-    create_dir(&app_dir)?;
-    create_dir(&app_dir.join(PARSED_FILES_DIR))?;
+    fs::create_dir(&app_dir)?;
+    fs::create_dir(&app_dir.join(PARSED_FILES_DIR))?;
 
-    let mut preset_file = File::create(app_dir.join(PRESETS_FILE_NAME))?;
-    let default_presets = read_to_string(DEFAULT_PRESETS_PATH)?;
+    let mut preset_file = fs::File::create(app_dir.join(PRESETS_FILE_NAME))?;
+    let default_presets = fs::read_to_string(DEFAULT_PRESETS_PATH)?;
 
     preset_file.write_all(default_presets.as_bytes())?;
 
     Ok(())
 }
 
-pub fn create_output_file() -> Result<File, String> {
+pub fn create_output_file() -> anyhow::Result<File> {
     let timestamp = Local::now().format("%Y-%m-%d_%H-%M-%S").to_string();
     let file_name = format!("{}.txt", timestamp);
-    let file_path = get_app_dir().map_err(|e| e.to_string())?.join(PARSED_FILES_DIR).join(file_name);
-
-    File::create(file_path).map_err(|e| e.to_string())
+    let file_path = get_app_dir()?.join(PARSED_FILES_DIR).join(file_name);
+    let file = fs::File::create(file_path)?;
+    Ok(file)
 }
 
-pub fn write_parsed_files(paths: Vec<PathBuf>, output_file: &mut File) -> Result<(), String> {
-    for path in paths {
-        match read_to_string(&path) {
-            Ok(content) => {
-                if let Err(e) = writeln!(output_file, "\n===== {} =====\n{}", path.display(), content) {
-                    eprint!("⚠️ Failed to write to output file: {}", e);
-                }
-            },
-            Err(e) => {
-                eprintln!("⚠️ Skipping '{}': {}", path.display(), e);
-            }
-        }
-    }
+pub fn write_parsed_files(paths: Vec<PathBuf>, output_file: &mut File) -> anyhow::Result<()> {
+    let combined_content = paths
+        .into_iter()
+        .filter_map(|path| {
+            fs::read_to_string(&path)
+                .map(|content| format!("===== {} =====\n{}", path.display(), content))
+                .ok()
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    writeln!(output_file, "{}", combined_content)?;
 
     Ok(())
 }
 
-
-pub fn get_file_preview(file_path: PathBuf) -> Result<FilePreview, String> {
-    let meta = metadata(&file_path).map_err(|e| e.to_string())?;
-    let bytes = read(&file_path).map_err(|e| e.to_string())?;
+pub fn get_file_preview(file_path: PathBuf) -> anyhow::Result<FilePreview> {
+    let meta = fs::metadata(&file_path)?;
+    let bytes = fs::read(&file_path)?;
 
     let preview = String::from_utf8_lossy(&bytes)
         .lines()
@@ -77,7 +77,7 @@ pub fn get_file_preview(file_path: PathBuf) -> Result<FilePreview, String> {
 
     let name = file_path
         .file_name()
-        .ok_or("Failed to extract filename")?
+        .ok_or(anyhow::anyhow!("Failed to extract filename"))?
         .to_string_lossy()
         .to_string();
 
@@ -89,7 +89,24 @@ pub fn get_file_preview(file_path: PathBuf) -> Result<FilePreview, String> {
     })
 }
 
-pub fn parse_paths(paths: Vec<String>, ignore_patterns: Vec<String>) -> Result<Vec<PathBuf>, globset::Error> {
+pub fn parse_paths(
+    paths: Vec<String>,
+    ignore_patterns: Vec<String>,
+) -> anyhow::Result<Vec<PathBuf>> {
+    fn collect_files(directory: &PathBuf, collected: &mut Vec<PathBuf>) {
+        if let Ok(entries) = fs::read_dir(directory) {
+            for entry in entries.flatten() {
+                let path = entry.path();
+
+                if path.is_file() {
+                    collected.push(path);
+                } else if path.is_dir() {
+                    collect_files(&path, collected);
+                }
+            }
+        }
+    }
+
     let filtered_paths = filter_by_preset(paths, &ignore_patterns)?;
     let mut collected_paths = Vec::new();
 
@@ -97,7 +114,7 @@ pub fn parse_paths(paths: Vec<String>, ignore_patterns: Vec<String>) -> Result<V
         let path = PathBuf::from(entry);
 
         if path.is_file() {
-                collected_paths.push(path);
+            collected_paths.push(path);
         } else if path.is_dir() {
             collect_files(&path, &mut collected_paths)
         }
@@ -106,24 +123,26 @@ pub fn parse_paths(paths: Vec<String>, ignore_patterns: Vec<String>) -> Result<V
     Ok(collected_paths)
 }
 
-pub fn get_app_dir() -> Result<PathBuf, String> {
-    home_dir()
-        .map(|dir| dir.join(APP_NAME))
-        .ok_or("Failed to get home directory".to_string())
+pub fn get_app_dir() -> anyhow::Result<PathBuf> {
+    let home_dir = dirs::home_dir()
+        .ok_or(anyhow::anyhow!("Can't access home dir"))?
+        .join(APP_NAME);
+    Ok(home_dir)
 }
 
-pub fn get_presets_map() -> Result<HashMap<String, Vec<String>>, String> {
+pub fn get_presets_map() -> anyhow::Result<HashMap<String, Vec<String>>> {
     let path = get_app_dir()?.join(PRESETS_FILE_NAME);
-    let content = read_to_string(path).map_err(|e| e.to_string())?;
-    let map = from_str::<HashMap<String, Vec<String>>>(&content).map_err(|e| e.to_string())?;
+    let content = fs::read_to_string(path)?;
+    let map = serde_json::from_str::<HashMap<String, Vec<String>>>(&content)?;
 
     Ok(map)
 }
 
-pub fn write_presets(map: &HashMap<String, Vec<String>>) -> Result<(), String> {
+pub fn write_presets(map: &HashMap<String, Vec<String>>) -> anyhow::Result<()> {
     let path: PathBuf = get_app_dir()?.join(PRESETS_FILE_NAME);
-    let json = to_string_pretty(map).map_err(|e| e.to_string())?;
-    write(path, json).map_err(|e| e.to_string())?;
+    let json = serde_json::to_string_pretty(map)?;
+    fs::write(path, json)?;
+
     Ok(())
 }
 
@@ -155,21 +174,10 @@ pub fn open_with_default_app(path: &Path) -> Result<(), String> {
     Ok(())
 }
 
-fn collect_files(directory: &PathBuf, collected: &mut Vec<PathBuf>) {
-    if let Ok(entries) = read_dir(directory) {
-        for entry in entries.flatten() {
-            let path = entry.path();
-
-            if path.is_file() {
-                collected.push(path);
-            } else if path.is_dir() {
-                collect_files(&path, collected);
-            }
-        }
-    }
-}
-
-fn filter_by_preset(file_paths: Vec<String>, patterns: &[String]) -> Result<Vec<String>, globset::Error> {
+fn filter_by_preset(
+    file_paths: Vec<String>,
+    patterns: &[String],
+) -> anyhow::Result<Vec<String>> {
     let mut builder = GlobSetBuilder::new();
 
     for pattern in patterns {
