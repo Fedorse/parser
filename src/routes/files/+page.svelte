63575eb9d5643fc371cfac6b84f14bd3e2ce2f44
Fedor Semochkin <221800ss@gmail.com>
2025-06-16 13:33:29 +0700

    update card

--- src/routes/files/+page.svelte
+++ src/routes/files/+page.svelte
@@ -5,16 +5,18 @@
     import Delete from '@lucide/svelte/icons/trash-2'
     import OpenFolder from '@lucide/svelte/icons/folder-open-dot'
     import File from '@lucide/svelte/icons/file-text'
-    import Edit from '@lucide/svelte/icons/pencil'
     import Input from '$lib/components/ui/input/input.svelte';
 	import Badge from '$lib/components/ui/badge/badge.svelte';
-    import * as Tabs from "$lib/components/ui/tabs/index.js";
     import * as Dialog from "$lib/components/ui/dialog/index.js";
-    import { Separator } from '$lib/components/ui/separator/index.js';
     import Code2 from '@lucide/svelte/icons/code-2'
     import Code from '@lucide/svelte/icons/code'
     import { Copy } from '@lucide/svelte/icons';
+    import * as Tooltip from "$lib/components/ui/tooltip/index.js";
 
+
+
+
+    const THIRTY_MB_SIZE = 30 * 1024 * 1024;
 
 
     type SavedFiles = {
@@ -29,11 +31,13 @@
     let rename = $state<string | null>(null)
     let isCodeDialogOpen = $state(false);
     let fileContent = $state<string>('');
-    let selectedFile = $state(null)
+    let selectedFile = $state<SavedFiles | null>(null);
+    let isCopied = $state(false);
+
 
     
 
-    const openDialogCode = async (file)=>{
+    const openDialogCode = async (file: SavedFiles)=>{
         selectedFile = file
         isCodeDialogOpen = true
         try {
@@ -86,12 +90,33 @@
     const updateFileContent = async (content: string) => {
         isCodeDialogOpen = false
         try {
-            await invoke('update_file', { filePath: selectedFile.path, content: content })            
+            await invoke('update_file', { filePath: selectedFile?.path, content: content })            
             loadFiles()
         } catch (err) {
             console.error('Failed to update file content:', err);
         }
     }
+    
+    const openDefaultEditor = async (path: string) => {
+        try {
+            await invoke('open_in_default_editor', { filePath: path });
+        } catch (err) {
+            console.error('Failed to open file in editor:', err);
+        }
+    }
+
+    const handleCopy = async (content: string) => {
+        try {
+            await navigator.clipboard.writeText(content);
+            isCopied = true
+            setTimeout(()=> isCopied = false, 2000)
+        } catch (err) {
+            console.error('Failed to copy content:', err);
+        }
+
+    }
+
+
 
     export const formatFileSize = (bytes: number): string => {
 	if (bytes < 1024) return `${bytes} B`;
@@ -118,7 +143,7 @@
           <Card.Header>
             <Card.Title class='flex items-center gap-4 justify-between ' >
                 {#if rename === file.path}
-                <Input class='flex-1' autofocus  bind:value={newName} onkeydown={(e) => {
+                <Input class='flex-1' autofocus  bind:value={newName}  onkeydown={(e) => {
                     if(e.key === 'Enter') handleRename(file)
                     else if(e.key === 'Escape') rename = null;
                 }}
@@ -129,45 +154,49 @@
             />
                 <Button variant='default' size='sm' onclick={()=>{handleRename(file)}}>Saved</Button>
                 {:else}
-              <span class="flex-1 max-w-64 truncate">{file.name}</span>
-              <Button
-                variant="link"
-                size="sm"
-                onclick={() => {
-                  rename = file.path;
-                  newName = file.name;
-                }}
-              >
-                <Edit class="size-3" /> Edit
-              </Button>
+              <span class="flex-1 max-w-64 truncate" onclick={()=> {
+                rename = file.path
+                newName = file.name
+            }}>{file.name} </span>
                 {/if}
             </Card.Title>
             <Card.Description >
-                <Badge variant='outline' class='text-muted-foreground'  >
+                <Badge variant='outline' class='text-muted-foreground'>
                     {formatFileSize(file.size)}
                 </Badge>
                 </Card.Description>
           </Card.Header>
           <Card.Content class='min-h-80 overflow-hidden'>
-            <p class="line-clamp-[13] text-muted-foreground">{file.preview}</p>
+            <p class="line-clamp-[13] text-balance">{file.preview}</p>
           </Card.Content>
           <Card.Footer class='flex justify-between w-full border-t border-text-muted-foreground/20  '>
-            <div>
-                <Tabs.Root value="preview" >
-                    <Tabs.List>
-                      <Tabs.Trigger value="preview">Prewiew</Tabs.Trigger>
-                      <Tabs.Trigger value="code" onclick={()=>openDialogCode(file)}>Code <Code/></Tabs.Trigger>
-                    </Tabs.List>
-                  </Tabs.Root>
+            <div class="flex gap-2">
+                <Button variant='outline' size='sm' onclick={()=> {
+                    if (file.size > THIRTY_MB_SIZE) {
+                        openDefaultEditor(file.path)
+                    } else {
+                   openDialogCode(file)} 
+
+                }}>
+                    <Code class="h-4 w-4 mr-2" />
+                    Edit
+                </Button>
+                <Button variant='ghost' size='sm' onclick={()=> handleOpenDir(file)}>
+                    <OpenFolder class="h-4 w-4 mr-2" />
+                    Open
+                </Button>
             </div>
+            
             <div class="flex"> 
-              <Button variant='link' onclick={()=> handleOpenDir(file)}><OpenFolder/></Button>
-              <Button variant='destructive' onclick={()=> handleDelete(file)}><Delete/></Button>
+              <Button variant='destructive' size='sm' onclick={()=> handleDelete(file)}>
+                <Delete class="h-4 w-4" />
+              </Button>
             </div>
           </Card.Footer>
         </Card.Root>
       {/each}
       </div>
+
           {#if savedFiles.length === 0} 
             <div class="text-center">
                 <File class='mx-auto mb-4 text-muted-foreground size-12'/>
@@ -188,16 +217,22 @@
                 <div class="flex-1 flex flex-col min-h-0">
                     <div class="flex justify-between items-center mb-3">
                         <Badge variant="secondary">
-                            {selectedFile.path}
+                            {selectedFile?.path}
                         </Badge>
                         <Button 
                             variant="outline" 
                             size="sm" 
+                            onclick={()=>handleCopy(fileContent)}
 
-                        >
+                        >{#if !isCopied}
                             <Copy class="h-4 w-4 mr-2" />
                             Copy
-                        </Button>
+                        {:else}
+                        <Copy class="h-4 w-4 mr-2 text-green-700" />
+                        <span class="text-green-700">
+                            Copied
+                        </span>
+                        {/if}</Button>
                     </div>
                     
                     <div class="flex-1 min-h-0 border rounded-md bg-muted/50 ">
